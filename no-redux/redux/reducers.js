e5ad3b6602b5dc6db5f80354c7edea8e4ef42a11
Nikolai <n.darinsky@gmail.com>
2020-01-22 01:29:50 +0300

    Custom Redux| Implement reducer

---
+++ no-redux/redux/reducers.js
@@ -0,0 +1,44 @@
+const CREATE_NOTE = 'CREATE_NOTE';
+const UPDATE_NOTE = 'UPDATE_NOTE';
+
+/**
+ * PURE functions. NEVER change\mutate state directly.
+ * Don't change the state. Instead, create shallow copies of the state and any nested objects/arrays
+ * @param {*} state 
+ * @param {*} action 
+ */
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case CREATE_NOTE: {
+        const id = state.nextNoteId;
+        const newNote = {
+          id,
+          content: ''
+        };
+        return {
+          ...state,
+          nextNoteId: id + 1,
+          notes: {
+            ...state.notes,
+            [id]: newNote
+          }
+        };
+      }
+      case UPDATE_NOTE: {
+        const {id, content} = action;
+        const editedNote = {
+          ...state.notes[id],
+          content
+        };
+        return {
+          ...state,
+          notes: {
+            ...state.notes,
+            [id]: editedNote
+          }
+        };
+      }
+      default:
+        return state;
+    }
+  };
