020e8f4e43ef24aef181e748053a4c60f07059b0
Nikolai <n.darinsky@gmail.com>
2020-01-22 01:53:44 +0300

    Custom redux| add subscribers to store

--- no-redux/src/redux/store.js
+++ no-redux/src/redux/store.js
@@ -11,13 +11,26 @@ const validateAction = action => {
 
 const createStore = (reducer) => {
     let state = undefined
-    return {
-        dispatch: (action) => {
-          validateAction(action)
-            state = reducer(state, action)
-        },
-        getState: () => state 
-    }
+    const subscribers = [];
+    const store = {
+      dispatch: action => {
+        validateAction(action);
+        state = reducer(state, action);
+        subscribers.forEach(handler => handler());
+      },
+      getState: () => state,
+      subscribe: handler => {
+        subscribers.push(handler);
+        return () => {
+          const index = subscribers.indexOf(handler);
+          if (index > 0) {
+            subscribers.splice(index, 1);
+          }
+        };
+      }
+    };
+    store.dispatch({type: '@@redux/INIT'});
+    return store;
 }
 
 export default createStore;
