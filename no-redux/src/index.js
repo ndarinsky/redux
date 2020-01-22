26cf128e3a387d4ec9014cb0eb4edf3a464c10e5
Nikolai Darinsky <n.darinsky@gmail.com>
2020-01-22 17:38:40 +0300

    Custom redux| Add middleware support

--- no-redux/src/index.js
+++ no-redux/src/index.js
@@ -6,7 +6,36 @@ import reducer from "./redux/reducers";
 import createStore from "./redux/store";
 import Provider from "./redux/provider";
 
-const store = createStore(reducer);
+const delayMiddleware = () => next => action => {
+  setTimeout(() => {
+    next(action);
+  }, 1000);
+}
+
+const loggingMiddleware = ({getState}) => next => action => {
+  console.info('before', getState());
+  console.info('action', action);
+  const result = next(action);
+  console.info('after', getState());
+  return result;
+};
+
+const applyMiddleware = (...middlewares) => store => {
+  if (middlewares.length === 0) {
+    return dispatch => dispatch;
+  }
+  if (middlewares.length === 1) {
+    return middlewares[0](store);
+  }
+  const boundMiddlewares = middlewares.map(middleware =>
+    middleware(store)
+  )
+  return boundMiddlewares.reduce((a, b) =>
+    next => a(b(next))
+  )
+}
+
+const store = createStore(reducer, applyMiddleware(delayMiddleware, loggingMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
